257bacace339dd30673edeac0a1b65eca83f9f71
rom4na4 <rom4na4@yandex.ru>
2023-10-10 00:07:23 +0900

    Added task solution for lesson 5 (Part 2) (Correction 1)

--- src/Lesson5_2.java
+++ src/Lesson5_2.java
@@ -5,49 +5,45 @@ public class Lesson5_2 {
         checkYear(2004);
         int currentYear = LocalDate.now().getYear();
         checkSystem(currentYear);
-        delivery(30);
+        System.out.println(checkDeliveryTime(30));
     }
 
     public static void checkYear(int year) {
         System.out.println("1 Task");
-        if (year % 4 == 0){
-            System.out.println(year +" год — високосный год");
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
         } else {
-            System.out.println(year +" год — невисокосный год");
+            System.out.println(year + " год — невисокосный год");
         }
         System.out.print("\n");
     }
 
     public static void checkSystem(int year) {
         System.out.println("2 Task");
+        int currentYear = LocalDate.now().getYear();
         int clientOS = 0;
-        if (clientOS == 0){
-            if (year < 2015){
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            } else if(year >= 2015){
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-            }
-        } else if (clientOS == 1){
-            if (year < 2015){
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else if(year >= 2015){
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            }
+        if (clientOS == 0 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOS == 0 && year == currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1 && year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && year == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
         }
         System.out.print("\n");
     }
 
-    public static void delivery(int distance) {
+    public static String checkDeliveryTime(int distance) {
         System.out.println("3 Task");
         if (distance < 20) {
-            System.out.println("Потребуется дней: " + 1);
-        } else if(distance >= 20 && distance < 60){
-            System.out.println("Потребуется дней: " + 2);
-        } else if(distance >= 60 && distance <= 100){
-            System.out.println("Потребуется дней: " + 3);
-        } else if(distance > 100){
-            System.out.println("Доставки нет.");
+            return "Потребуется дней: " + 1;
+        } else if (distance >= 20 && distance < 60) {
+            return "Потребуется дней: " + 2;
+        } else if (distance >= 60 && distance <= 100) {
+            return "Потребуется дней: " + 3;
+        } else {
+            return "Доставки нет.";
         }
-        System.out.print("\n");
     }
 }
